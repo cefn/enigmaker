ddb7ae3b53d533bddafb1029ea664e6fec6aa4cf
Cefn Hoile <github.com@cefn.com>
2010-02-06 00:21:38 +0000

    Combined code in project0 sketch, added motion switch for testing

--- project0/applet/project0.cpp
+++ project0/applet/project0.cpp
@@ -1,4 +1,8 @@
 #include <Tone.h>
+#include <Wire.h>
+
+#define DEVICE (0x53)    //ADXL345 device address
+#define TO_READ (6)        //num of bytes we are going to read each time (two bytes for each axis)
 
 #include "WProgram.h"
 void setup();
@@ -6,14 +10,22 @@ void loop();
 void displayDigit(int digit);
 void countUp();
 void countDown();
-boolean popEvent();
+void soundMip();
 void soundAlarm();
 void silenceAlarm();
 void serviceAlarm();
+char *ftoa(char *a, double f, int precision);
+void writeTo(int device, byte address, byte val);
+void readFrom(int device, byte address, int num, byte buff[]);
+byte buff[TO_READ] ;    //6 bytes buffer for saving data read from the device
+char str[256];                      //string buffer to transform data before sending it to the serial port
+char pythagstr[16];                      //string buffer to transform data before sending it to the serial port
+
 int numDisplayPins = 8;
 
 int audioPin = 9; //has to be a PWM pin set to output
 int buttonPin = 12; //has to be set to an input pin
+int switchPin = 8; //has to be set to an input pin
 int displayPins[] = { //have to be set to output
   7,6,4,3,2,10,11,5
 }; 
@@ -50,16 +62,21 @@ byte digitSequence[] = {
 //only works for 0-9 for now, cld use A, b, C, D, E F to display other info.
 int base = 10;
 
+//Button debounce logic
 boolean buttonCircuit,lastButtonCircuit;
-
 long circuitIdle, circuitDebounce = 50;
 long lastCircuitChange;
-
 boolean buttonDown,lastButtonDown;
-
 long lastButtonChange, buttonHoldPeriod;
 
-long lastEvent;
+//Motion debounce logic
+boolean motionLow, lastMotionLow;
+long motionIdle, motionDebounce = 1000;
+long lastMotionChange;
+boolean motionStopped, lastMotionStopped;
+long lastStopChange, nonStopPeriod;
+
+boolean switchCircuit;
 
 Tone tone;
 boolean alarm;
@@ -74,9 +91,20 @@ int mode = CONFIGURATION_MODE;
 int number = -1;
 
 void setup(){
+  
+  Wire.begin();        // join i2c bus (address optional for master)
+  Serial.begin(9600);  // start serial for output
+  
+  //Turning on the ADXL345
+  writeTo(DEVICE, 0x2D, 0);      
+  writeTo(DEVICE, 0x2D, 16);
+  writeTo(DEVICE, 0x2D, 8);
+  
   //configure pins in correct modes
-  pinMode(audioPin, OUTPUT);
   pinMode(buttonPin, INPUT);
+  pinMode(switchPin, INPUT);
+
+  pinMode(audioPin, OUTPUT);
   for (int i = 0; i < numDisplayPins; i++) {
     pinMode(displayPins[i], OUTPUT);  
   }  
@@ -86,9 +114,70 @@ void setup(){
   
   //configure tone library for speaker pin
   tone.begin(audioPin);
+  
+  
 }
 
 void loop(){
+  
+  int regAddress = 0x32;    //first axis-acceleration-data register on the ADXL345
+  int x, y, z;
+  float fx, fy, fz;
+  double a;
+  
+  readFrom(DEVICE, regAddress, TO_READ, buff); //read the acceleration data from the ADXL345
+  
+   //each axis reading comes in 10 bit resolution, ie 2 bytes.  Least Significat Byte first!!
+   //thus we are converting both bytes in to one int
+  x = (((int)buff[1]) << 8) | buff[0];   
+  y = (((int)buff[3])<< 8) | buff[2];
+  z = (((int)buff[5]) << 8) | buff[4];
+  fx = float(x);
+  fy = float(y);
+  fz = float(z);
+  a = sqrt((fx*fx) + (fy*fy) + (fz*fz));
+  
+  if(a < 0.02f){
+    if(motionLow != lastMotionLow){
+      motionLow = true;
+      lastMotionChange = millis();
+    }
+  }
+  else {
+    motionStopped = false;
+  }
+  lastMotionLow = motionLow;
+  
+  motionIdle = millis() - lastMotionChange;
+  if(motionIdle > motionDebounce){
+    if(motionStopped != motionLow){
+      motionStopped = motionLow;
+      nonStopPeriod = millis() - lastMotionChange;
+      lastStopChange = millis();
+    }
+  }
+
+  switchCircuit = digitalRead(switchPin);
+  
+  if(motionStopped && (motionStopped != lastMotionStopped)){
+    soundMip();
+    countDown();
+  }
+  lastMotionStopped = motionStopped;
+  
+  ftoa(pythagstr,a,2);
+  Serial.print(pythagstr);
+  Serial.print(', ');
+  
+  //we send the x y z values as a string to the serial port
+  sprintf(str, ", %d, %d, %d", x, y, z);  
+  Serial.print(str);
+  sprintf(str, ", %d", switchCircuit);  
+  Serial.print(str);
+  Serial.print('\n');
+  
+  //It appears that delay is needed in order not to clog the port
+  //delay(15); 
   
   //read button state and record change
   buttonCircuit = digitalRead(buttonPin) == HIGH;
@@ -106,6 +195,11 @@ void loop(){
       lastButtonChange = millis();
     }
   }
+
+  if( buttonDown && (buttonDown != lastButtonDown)){ //button pushed
+    Serial.println("BUTTON PUSHED");  
+  }
+
 
   if( (!buttonDown) && (buttonDown != lastButtonDown)){ //button released
     if(buttonHoldPeriod > 1000){//long period switches mode 
@@ -126,9 +220,10 @@ void loop(){
   
   if(mode == OPERATION_MODE){
     //trigger a countdown if appropriate
-    if(popEvent()){
-      countDown();
-    }
+    //used to fake events before accelerometer attached
+    //if(popEvent()){
+    //  countDown();
+    //}
     if(number < 2){
       soundAlarm();
     }
@@ -144,7 +239,7 @@ void loop(){
   displayDigit(number);
 
   serviceAlarm();
-  
+    
 }
 
 /** Displays the specified digit. */
@@ -185,14 +280,9 @@ void countDown(){
   }
 }
 
-/** Has the thing we've been monitoring for happened at least once? */
-boolean popEvent(){
-  if(millis() - lastEvent > 3000){
-    lastEvent = millis();
-    return true;
-  }
-  else{
-    return false;
+void soundMip(){
+  if(!alarm){
+    tone.play(NOTE_C8,50);
   }
 }
 
@@ -214,6 +304,47 @@ void serviceAlarm(){
   else{
     tone.stop();
   }
+}
+
+char *ftoa(char *a, double f, int precision)
+{
+  long p[] = {0,10,100,1000,10000,100000,1000000,10000000,100000000};
+  
+  char *ret = a;
+  long heiltal = (long)f;
+  itoa(heiltal, a, 10);
+  while (*a != '\0') a++;
+  *a++ = '.';
+  long desimal = abs((long)((f - heiltal) * p[precision]));
+  itoa(desimal, a, 10);
+  return ret;
+}
+
+//---------------- Functions
+//Writes val to address register on device
+void writeTo(int device, byte address, byte val) {
+   Wire.beginTransmission(device); //start transmission to device 
+   Wire.send(address);        // send register address
+   Wire.send(val);        // send value to write
+   Wire.endTransmission(); //end transmission
+}
+
+//reads num bytes starting from address register on device in to buff array
+void readFrom(int device, byte address, int num, byte buff[]) {
+  Wire.beginTransmission(device); //start transmission to device 
+  Wire.send(address);        //sends address to read from
+  Wire.endTransmission(); //end transmission
+  
+  Wire.beginTransmission(device); //start transmission to device
+  Wire.requestFrom(device, num);    // request 6 bytes from device
+  
+  int i = 0;
+  while(Wire.available())    //device may send less than requested (abnormal)
+  { 
+    buff[i] = Wire.receive(); // receive a byte
+    i++;
+  }
+  Wire.endTransmission(); //end transmission
 }
 
 int main(void)
